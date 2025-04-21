14b7ec391920a11bca740ee1c45c12e027c3586d
kevinw607 <lolexolalv@gmai.com>
2025-04-21 13:30:21 -0400

    Avances clases arreglos

---
+++ arreglos.py
@@ -0,0 +1,15 @@
+#alo 
+
+
+
+#             0         1           2             
+#frutas = ["Manzana", "Pera" , "Frutilla"]
+#print(frutas[2])
+
+
+
+estudiantes = [{'nombre' : "Maxi", 'edad' : 20, 'curso' : "2a"}, 
+               {'nombre' : "Richard", 'edad' : 19, 'curso' : "1b" } , 
+               {'nombre' : "kevin", 'edad' : 20 , 'curso' : "3c"}]
+
+print(estudiantes[0]['nombre'])
